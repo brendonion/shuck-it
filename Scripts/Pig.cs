9db95c6ad4739122b455f9ba2c99f700279c91d6
Brendan Walker <bwalker@appcolony.ca>
2020-10-28 16:52:08 -0700

    Adds more functionality to Pig (WIP).

--- Scripts/Pig.cs
+++ Scripts/Pig.cs
@@ -1,30 +1,63 @@
 using Godot;
+using System;
 
 public class Pig : Node2D {
 
-    public Vector2 startPos;
+    public float speed = 125f;
+
+    public int patrolIndex = 0;
+
+    public Game Game;
 
     public KinematicBody2D body;
 
     public AnimatedSprite animatedSprite;
 
+    public Path2D[] paths;
+
+    public Path2D patrolPath;
+
+    public Vector2[] patrolPoints;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() {
         GD.Randomize();
 
+        Path2D[] paths = {
+            (Path2D) FindNode("PigPath1"), // Bottom Left
+            (Path2D) FindNode("PigPath2"), // Bottom Right
+            (Path2D) FindNode("PigPath3"), // Top Left
+            (Path2D) FindNode("PigPath4"), // Top Right
+        };
+
+        this.paths          = paths;
+        this.patrolPath     = this.paths[(int) GD.RandRange(0, 4)];
+        this.patrolPoints   = this.patrolPath.Curve.GetBakedPoints();
         this.body           = (KinematicBody2D) FindNode("KinematicBody2D");
         this.animatedSprite = (AnimatedSprite) this.body.FindNode("AnimatedSprite");
-        this.startPos       = this.body.Position;
 
-        int num = (int) GD.RandRange(0, 2);
-        // Spawn bottom left or bottom right
-        if (num == 0) {
-            // this
-        }
+        // this.Game = (Game) GetTree().Root.GetChild(0);
 
+        this.DetermineStartPosition();
     }
 
     public override void _PhysicsProcess(float delta) {
+        Vector2 target   = this.patrolPoints[this.patrolIndex];
+        Vector2 velocity = (target - this.body.Position).Normalized() * this.speed;
+        this.body.MoveAndSlide(velocity);
+        if (this.body.Position.DistanceTo(target) <= 2) {
+            if (this.patrolIndex == this.patrolPoints.Length - 1) {
+                this.patrolIndex = 0;
+            } else {
+                this.patrolIndex += 1;
+            }
+        }
+    }
 
+    public void DetermineStartPosition() {
+        int index = Array.IndexOf(this.paths, this.patrolPath);
+        this.body.Position = this.patrolPoints[0];
+        this.animatedSprite.FlipH = (index == 0 || index == 2);
+        this.animatedSprite.FlipV = (index == 2 || index == 3);
     }
 }
