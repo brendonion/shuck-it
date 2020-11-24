fb89af3b0a2a1dfcbb7879e246dd4da81250aab3
Brendan Walker <bwalker@appcolony.ca>
2020-11-24 14:25:00 -0800

    Adds popup confirmation when clicking on a shop item.

--- Scripts/Shop.cs
+++ Scripts/Shop.cs
@@ -2,9 +2,30 @@ using Godot;
 
 public class Shop : Control {
 
-    public override void _Ready() {}
+    public ScrollContainer scrollContainer;
+    
+    public ConfirmationDialog confirmation;
+
+    public override void _Ready() {
+        this.scrollContainer = (ScrollContainer) FindNode("ScrollContainer");
+        this.confirmation    = (ConfirmationDialog) FindNode("ConfirmationDialog");
+        this.confirmation.GetCloseButton().Visible = false;
+    }
 
     public void _OnBackPressed() {
         GetTree().ChangeScene("res://Scenes/Home.tscn");
+    }
+
+    public void _OnItemPressed() {
+        this.scrollContainer.SetProcessInput(false);
+        this.confirmation.PopupCentered();
+    }
+
+    public void _OnPopupHide() {
+        this.scrollContainer.SetProcessInput(true);
+    }
+
+    public void _OnPopupConfirmed() {
+        GD.Print("PURCHASED!");
     }
 }
