ec0c1c2749d923073a03026af50ea8bf42c032f6
Brendan Walker <brenny_walker@hotmail.com>
2020-11-25 20:06:43 -0800

    Adds start to proper save system.

--- Scripts/Home.cs
+++ Scripts/Home.cs
@@ -2,23 +2,21 @@ using Godot;
 
 public class Home : Node2D {
 
-    public string settingsFile = "user://settings.save";
-
-    public bool enableAds   = true;
-    public bool enableSound = true;
-
     public Texture soundOnTexture  = (Texture) ResourceLoader.Load("res://Art/SoundOn.png");
     public Texture soundOffTexture = (Texture) ResourceLoader.Load("res://Art/SoundOff.png");
 
     public Button adsButton;
     public Button soundButton;
 
+    public SaveSystem saveSystem;
+
     public override void _Ready() {
+        this.saveSystem = (SaveSystem) GetNode("/root/SaveSystem");
+
         this.adsButton   = (Button) FindNode("AdsButton");
         this.soundButton = (Button) FindNode("SoundButton");
 
-        this.LoadSettings();
-        this.RegisterButtons();
+        this.UpdateButtons();
     }
 
     public void _OnPlayPressed() {
@@ -32,50 +30,27 @@ public class Home : Node2D {
     public void _OnAdsPressed() {
         // this.enableAds = !this.enableAds;
         // this.SaveSettings();
-        // this.RegisterButtons();
+        // this.UpdateButtons();
     }
 
     public void _OnSoundPressed() {
-        this.enableSound = !this.enableSound;
-        this.SaveSettings();
-        this.RegisterButtons();
+        this.saveSystem.enableSound = !saveSystem.enableSound;
+        this.saveSystem.Save();
+        this.UpdateButtons();
     }
 
     public void _OnShopPressed() {
         GetTree().ChangeScene("res://Scenes/Shop.tscn");
     }
 
-    public void RegisterButtons() {
-        this.adsButton.Disabled = !this.enableAds;
-        if (this.enableSound) {
+    public void UpdateButtons() {
+        this.adsButton.Disabled = !this.saveSystem.enableAds;
+        if (this.saveSystem.enableSound) {
             this.soundButton.Icon = this.soundOnTexture;
             AudioServer.SetBusMute(0, false);
         } else {
             this.soundButton.Icon = this.soundOffTexture;
             AudioServer.SetBusMute(0, true);
         }
-    }
-
-    public void SaveSettings() {
-        var settings = new File();
-        settings.Open(settingsFile, File.ModeFlags.Write);
-
-        settings.StoreVar(this.enableAds);
-        settings.StoreVar(this.enableSound);
-
-        settings.Close();
-    }
-
-    public void LoadSettings() {
-        var settings = new File();
-        if (!settings.FileExists(this.settingsFile))
-            return;
-
-        settings.Open(settingsFile, File.ModeFlags.Read);
-
-        Set("enableAds", settings.GetVar());
-        Set("enableSound", settings.GetVar());
-
-        settings.Close();
     }
 }
---
+++ Scripts/SaveSystem.cs
@@ -0,0 +1,74 @@
+using Godot;
+
+public class SaveSystem : Node {
+
+    public const string CONFIG_FILE = "res://config.cfg";
+
+    // Settings sectiom
+    public bool enableAds = true;
+    public bool enableSound = true;
+
+    // Game section
+    public int bestScore = 0;
+    public int kernels = 0;
+
+    // Shop section
+    public string[] unlockedBackgrounds = {
+        "background_1.png"
+    };
+    public string[] unlockedSkins = {
+        "skin_1.png"
+    };
+    public string[] lockedBackgrounds = {
+        "background_2.png",
+        "background_3.png",
+        "background_4.png",
+        "background_5.png",
+    };
+    public string[] lockedSkins = {
+        "skin_2.png",
+        "skin_3.png",
+    };
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() {
+        Load();
+    }
+
+    public void Load() {
+        var config = new ConfigFile();
+        var err    = config.Load(CONFIG_FILE);
+
+        // If file doesn't exist then return
+        if (err != Error.Ok) return;
+
+        // Load data into member variables
+        Set("enableAds", config.GetValue("settings", "enable_ads"));
+        Set("enableSound", config.GetValue("settings", "enable_sound"));
+
+        Set("bestScore", config.GetValue("game", "best_score"));
+        Set("kernels", config.GetValue("game", "kernels"));
+
+        Set("unlockedBackgrounds", config.GetValue("shop", "unlocked_backgrounds"));
+        Set("unlockedSkins", config.GetValue("shop", "unlocked_skins"));
+        Set("unlockedBackgrounds", config.GetValue("shop", "locked_backgrounds"));
+        Set("unlockedSkins", config.GetValue("shop", "locked_skins"));
+    }
+
+    public void Save() {
+        var config = new ConfigFile();
+
+        config.SetValue("settings", "enable_ads", this.enableAds);
+        config.SetValue("settings", "enable_sound", this.enableSound);
+
+        config.SetValue("game", "best_score", this.bestScore);
+        config.SetValue("game", "kernels", this.kernels);
+
+        config.SetValue("shop", "unlocked_backgrounds", this.unlockedBackgrounds);
+        config.SetValue("shop", "unlocked_skins", this.unlockedSkins);
+        config.SetValue("shop", "locked_backgrounds", this.lockedBackgrounds);
+        config.SetValue("shop", "locked_skins", this.lockedSkins);
+
+        config.Save(CONFIG_FILE);
+    }
+}
