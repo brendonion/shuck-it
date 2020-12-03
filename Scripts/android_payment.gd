3ade999d47cdba4ff377fa3d5c17ae3ceb03b72c
Brendan Walker <bwalker@appcolony.ca>
2020-12-02 16:22:07 -0800

    Update.

--- Scripts/android_payment.gd
+++ Scripts/android_payment.gd
@@ -10,25 +10,25 @@ var item = "inapp"
 
 func _ready():
   if Engine.has_singleton("GodotGooglePlayBilling"):
-    payment = Engine.get_singleton("GodotGooglePlayBilling")
+		payment = Engine.get_singleton("GodotGooglePlayBilling")
 
-    # These are all signals supported by the API
-    # You can drop some of these based on your needs
-    payment.connect("connected", self, "_on_connected") # No params
-    payment.connect("disconnected", self, "_on_disconnected") # No params
-    payment.connect("connect_error", self, "_on_connect_error") # Response ID (int), Debug message (string)
-    payment.connect("purchases_updated", self, "_on_purchases_updated") # Purchases (Dictionary[])
-    payment.connect("purchase_error", self, "_on_purchase_error") # Response ID (int), Debug message (string)
-    payment.connect("sku_details_query_completed", self, "_on_sku_details_query_completed") # SKUs (Dictionary[])
-    payment.connect("sku_details_query_error", self, "_on_sku_details_query_error") # Response ID (int), Debug message (string), Queried SKUs (string[])
-    payment.connect("purchase_acknowledged", self, "_on_purchase_acknowledged") # Purchase token (string)
-    payment.connect("purchase_acknowledgement_error", self, "_on_purchase_acknowledgement_error") # Response ID (int), Debug message (string), Purchase token (string)
-    # payment.connect("purchase_consumed", self, "_on_purchase_consumed") # Purchase token (string)
-    # payment.connect("purchase_consumption_error", self, "_on_purchase_consumption_error") # Response ID (int), Debug message (string), Purchase token (string)
+		# These are all signals supported by the API
+		# You can drop some of these based on your needs
+		payment.connect("connected", self, "_on_connected") # No params
+		payment.connect("disconnected", self, "_on_disconnected") # No params
+		payment.connect("connect_error", self, "_on_connect_error") # Response ID (int), Debug message (string)
+		payment.connect("purchases_updated", self, "_on_purchases_updated") # Purchases (Dictionary[])
+		payment.connect("purchase_error", self, "_on_purchase_error") # Response ID (int), Debug message (string)
+		payment.connect("sku_details_query_completed", self, "_on_sku_details_query_completed") # SKUs (Dictionary[])
+		payment.connect("sku_details_query_error", self, "_on_sku_details_query_error") # Response ID (int), Debug message (string), Queried SKUs (string[])
+		payment.connect("purchase_acknowledged", self, "_on_purchase_acknowledged") # Purchase token (string)
+		payment.connect("purchase_acknowledgement_error", self, "_on_purchase_acknowledgement_error") # Response ID (int), Debug message (string), Purchase token (string)
+		# payment.connect("purchase_consumed", self, "_on_purchase_consumed") # Purchase token (string)
+		# payment.connect("purchase_consumption_error", self, "_on_purchase_consumption_error") # Response ID (int), Debug message (string), Purchase token (string)
 
-    payment.startConnection()
+		payment.startConnection()
   else:
-    print("Android IAP support is not enabled. Make sure you have enabled 'Custom Build' and the GodotGooglePlayBilling plugin in your Android export settings! IAP will not work.")
+		print("Android IAP support is not enabled. Make sure you have enabled 'Custom Build' and the GodotGooglePlayBilling plugin in your Android export settings! IAP will not work.")
 
 ### Event callbacks ###
 
@@ -47,7 +47,7 @@ func _on_connect_error(response_id, message):
 func _on_purchases_updated(purchases):
   print("GodotGooglePlayBilling PURCHASE UPDATED")
   for purchase in purchases:
-	  print("Purchase " + purchase)
+		print("Purchase " + purchase)
 
 func _on_purchase_error(response_id, message):
   print("GodotGooglePlayBilling PURCHASE ERROR")
@@ -57,14 +57,14 @@ func _on_purchase_error(response_id, message):
 func _on_sku_details_query_completed(sku_details):
   print("GodotGooglePlayBilling SKU DETAILS QUERY COMPLETED")
   for available_sku in sku_details:
-	  print("Available SKU: " + available_sku)
+		print("Available SKU: " + available_sku)
 
 func _on_sku_details_query_error(response_id, message, skus):
   print("GodotGooglePlayBilling SKU DETAILS QUERY ERROR")
   print("Response ID: " + response_id)
   print("Message: " + message)
   for sku_name in skus:
-	  print("SKU: "+ sku_name)
+		print("SKU: "+ sku_name)
 
 func _on_purchase_acknowledged(purchase_token):
   print("GodotGooglePlayBilling PURCHASE ACKNOWLEDGED")
